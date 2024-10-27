536a68f15b05ced1ad2a671d531072fcfe593edb
Aditya Gupta <anshuaditya230@gmail.com>
2024-10-27 21:26:03 +0530

    new change

--- otp_gen.py
+++ otp_gen.py
@@ -1,10 +1,23 @@
+"""
+This module provides functionality to generate a one-time password (OTP).
+"""
+
 import random
 import string
 
 def generate_otp(length=6):
+    """
+    Generates an OTP of a specified length.
+    
+    Args:
+        length (int): The length of the OTP to be generated (default is 6).
+        
+    Returns:
+        str: A string representing the generated OTP.
+    """
     characters = string.digits
-    otp = ''.join(random.choice(characters) 
-    for _ in range(length))
+    otp = ''.join(random.choice(characters) for _ in range(length))
     return otp
 
-print("Your OTP is:", generate_otp())
+if __name__ == "__main__":
+    print("Your OTP is:", generate_otp())
